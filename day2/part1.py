f1c78fc531793636be3d687cd63d540641a5231e
Hadrien Louis <hadry@protonmail.com>
2024-12-02 14:47:12 +0100

    fix

--- day2/part1.py
+++ day2/part1.py
@@ -5,7 +5,7 @@ lines = [list(map(int, line.split())) for line in data.strip().split("\n")]
 
 safe = 0
 for line in lines:
-    diffs = diffs = [line[i+1] - line[i] for i in range(len(line) - 1)]
+    diffs = [line[i+1] - line[i] for i in range(len(line) - 1)]
     if all(diff > 0 for diff in diffs) or all(diff < 0 for diff in diffs):
         if all(abs(diff) <= 3 for diff in diffs):
             safe += 1
